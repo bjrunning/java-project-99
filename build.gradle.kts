181312a805d12113f9c3fe1f3c89ff4ddf125f9f
bjrunning <senzhapov.02@mail.ru>
2023-12-13 18:04:42 +0300

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,13 +3,13 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 plugins {
     application
-    checkstyle
     jacoco
-    id("io.freefair.lombok") version "8.4"
-    id("io.sentry.jvm.gradle") version "4.0.0"
+    checkstyle
     id ("com.adarshr.test-logger") version "3.2.0"
+    id("io.freefair.lombok") version "8.4"
     id("org.springframework.boot") version "3.1.5"
     id("io.spring.dependency-management") version "1.1.3"
+    id("io.sentry.jvm.gradle") version "3.14.0"
 }
 
 group = "hexlet.code"
@@ -23,34 +23,6 @@ application {
     mainClass.set("hexlet.code.AppApplication")
 }
 
-dependencies {
-    implementation("org.mapstruct:mapstruct:1.5.5.Final")
-    annotationProcessor("org.mapstruct:mapstruct-processor:1.5.5.Final")
-
-    implementation("org.springframework.boot:spring-boot-starter")
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-validation")
-    implementation("org.springframework.boot:spring-boot-devtools")
-    implementation("org.springframework.boot:spring-boot-starter-security")
-    implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
-
-    implementation("net.datafaker:datafaker:2.0.1")
-    implementation("org.instancio:instancio-junit:3.3.0")
-    implementation("org.openapitools:jackson-databind-nullable:0.2.6")
-    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
-
-    runtimeOnly("com.h2database:h2")
-    runtimeOnly("org.postgresql:postgresql:42.6.0")
-
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-
-    testImplementation("org.springframework.security:spring-security-test")
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
-    testImplementation("net.javacrumbs.json-unit:json-unit-assertj:3.2.2")
-}
-
 buildscript {
     repositories {
         mavenCentral()
@@ -58,18 +30,34 @@ buildscript {
 }
 
 sentry {
-    // Generates a JVM (Java, Kotlin, etc.) source bundle and uploads your source code to Sentry.
-    // This enables source context, allowing you to see your source
-    // code as part of your stack traces in Sentry.
-    includeSourceContext = true
+    includeSourceContext.set(true)
 
-    org = "bjrunning"
-    projectName = "java-spring-boot"
-    authToken = System.getenv("SENTRY_AUTH_TOKEN")
+    org.set("bjrunning")
+    projectName.set("java-spring-boot")
+    authToken.set(System.getenv("SENTRY_AUTH_TOKEN"))
 }
 
-tasks.sentryBundleSourcesJava {
-    enabled = System.getenv("SENTRY_AUTH_TOKEN") != null
+dependencies {
+    implementation("org.mapstruct:mapstruct:1.5.5.Final")
+    annotationProcessor("org.mapstruct:mapstruct-processor:1.5.5.Final")
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-devtools")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
+    runtimeOnly("com.h2database:h2")
+    runtimeOnly("org.postgresql:postgresql:42.6.0")
+    implementation("net.datafaker:datafaker:2.0.1")
+    implementation("org.instancio:instancio-junit:3.3.0")
+    implementation("org.openapitools:jackson-databind-nullable:0.2.6")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.security:spring-security-test")
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
+    testImplementation("net.javacrumbs.json-unit:json-unit-assertj:3.2.2")
 }
 
 tasks.test {
@@ -85,4 +73,8 @@ tasks.jacocoTestReport {
     reports {
         xml.required = true
     }
+}
+
+tasks.sentryBundleSourcesJava {
+    enabled = System.getenv("SENTRY_AUTH_TOKEN") != null
 }
